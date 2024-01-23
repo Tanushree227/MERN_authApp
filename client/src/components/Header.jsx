1bf3ead979f5f553a23af6f554a00ed25461a044
Tanushree227 <tanushreedas813@gmail.com>
2024-01-23 19:15:33 +0530

    modifying home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
+import classes from "./Header.module.css";
 
 function Header()
 {
     return(
-        <div className="bg-slate-200">
+        // <div className="bg-slate-200">
+        //     <div className="flex justify-between items-center max-w-6xl mx-auto p-6">
+        //         <Link to='/'><h1 className="font-bold text-2xl">Reflect</h1></Link>
+        //         <ul className="flex gap-4">
+        //             <Link to='/'><li className="text-2xl">Home</li></Link>
+        //             <Link to='/about'><li className="text-2xl">About</li></Link>
+        //             <Link to='/sign-in'><li className="text-2xl">Sign-In</li></Link>
+        //         </ul>
+        //     </div>
+        // </div>
+        <header className={classes.header}>
             <div className="flex justify-between items-center max-w-6xl mx-auto p-6">
-                <Link to='/'><h1 className="font-bold text-2xl">Reflect</h1></Link>
-                <ul className="flex gap-4">
+            <h1 className={classes.logo}>EchoHub</h1>
+            <nav>
+                <ul>
                     <Link to='/'><li className="text-2xl">Home</li></Link>
                     <Link to='/about'><li className="text-2xl">About</li></Link>
                     <Link to='/sign-in'><li className="text-2xl">Sign-In</li></Link>
                 </ul>
-            </div>
+            </nav>
         </div>
+        </header>
+
     );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,37 @@
+const DUMMY_DATA = [
+    { 
+        id: 's1',
+        title: 'My Summer Vacation at Santorini',
+        image: 'https://images.unsplash.com/photo-1561642993-8621fcf54e76?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name: 'Christiano Pinto',
+        description: 'An island in the Aegean Sea, Santorini is known for its pristine whitewashed houses spread across jagged cliffs, astonishing blue waters, dramatic views, fabulous sunsets, the ancient settlement of Thira and an active volcano.'
+    },
+    { 
+        id: 's2',
+        title: 'My Summer Vacation at Santorini',
+        image: 'https://images.pexels.com/photos/163864/santorini-oia-greece-travel-163864.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        name: 'Christiano Pinto',
+        description: 'An island in the Aegean Sea, Santorini is known for its pristine whitewashed houses spread across jagged cliffs, astonishing blue waters, dramatic views, fabulous sunsets, the ancient settlement of Thira and an active volcano.'
+    },
+    { 
+        id: 's3',
+        title: 'My Summer Vacation at Santorini',
+        image: 'https://images.pexels.com/photos/2249780/pexels-photo-2249780.jpeg?auto=compress&cs=tinysrgb&w=600',
+        name: 'Christiano Pinto',
+        description: 'An island in the Aegean Sea, Santorini is known for its pristine whitewashed houses spread across jagged cliffs, astonishing blue waters, dramatic views, fabulous sunsets, the ancient settlement of Thira and an active volcano.'
+    }
+]
+
 function Home() {
     return(
-        <div>Home Page</div>
+        <section>
+            <h1>All Posts</h1>
+            <ul>
+            {DUMMY_DATA.map((posts) => {
+                return <li key={posts.id}>{posts.title}</li>
+            })}
+            </ul>
+        </section>
     )
 }
   
