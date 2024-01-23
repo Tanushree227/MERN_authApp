e3668884b3cc7476108072a0340d0d784769314b
Tanushree227 <tanushreedas813@gmail.com>
2024-01-23 12:14:36 +0530

    started with redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function SignIn() {
 
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+
+    const { loading, error } = useSelector((state) => state.user);
     
     const navigate = useNavigate();
+
+    const dispatch = useDispatch();
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value});
@@ -16,8 +20,7 @@ function SignIn() {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        setLoading(true);
-        setError(false);
+        dispatch(signInStart());
         const res = await fetch('/api/auth/signin', {
             method: 'POST',
             headers: {
@@ -26,16 +29,16 @@ function SignIn() {
             body: JSON.stringify(formData)
         });
         const data = await res.json();
-        setLoading(false);
+        dispatch(signInSuccess(data));
         if(data.success === false)
         {
-          setError(true);
+          dispatch(signInFailure(data));
           return;
         }
+        dispatch(signInSuccess(data));
         navigate('/')
       } catch (error) {
-        setLoading(false)
-        setError(true);
+        dispatch(signInFailure(error));
       }
         
     }
@@ -71,7 +74,7 @@ function SignIn() {
           <span className="text-blue-500">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && 'Something went wrong'}</p>
+      <p className="text-red-700 mt-5">{error ? error.message || 'Something went wrong' : ''}</p>
     </div>
   );
 }
