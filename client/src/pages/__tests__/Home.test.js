2169a1a5bafcb35280334481214eff1628c1247a
Tanushree227 <tanushreedas813@gmail.com>
2024-01-30 20:39:02 +0530

    modified home page testing

--- client/src/pages/__tests__/Home.test.js
+++ client/src/pages/__tests__/Home.test.js
@@ -6,15 +6,11 @@ import '@testing-library/jest-dom/extend-expect'; // for additional matchers
 test('Home Component renders posts after fetching data', async () => {
   const { getByText, queryByText, getByTestId, wait } = render(<Home />);
 
-  // Check if loading text is displayed initially
   expect(getByText(/loading/i)).toBeTruthy();
 
-  // Wait for the data to be loaded (assuming it takes some time)
   await wait(() => {
-    // Check if the loading text is not present
     expect(queryByText(/loading/i)).toBeNull();
 
-    // Check if the h1 element with text 'All Posts' is present
     expect(getByText(/All Posts/i)).toBeTruthy();
 
     // Check if at least one post is rendered
