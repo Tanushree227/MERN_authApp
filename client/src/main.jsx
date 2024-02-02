fdefa02bbb967e24a1ccf9a6e6e88909dea28ad3
Tanushree227 <tanushreedas813@gmail.com>
2024-02-02 10:38:44 +0530

    formatted the doc

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { FavoritesContextProvider } from './store/favorites-context.jsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { FavoritesContextProvider } from "./store/favorites-context.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate persistor={persistor} loading = {null}>
-  <FavoritesContextProvider>
-      
+    <PersistGate persistor={persistor} loading={null}>
+      <FavoritesContextProvider>
         <App />
-  </FavoritesContextProvider>
+      </FavoritesContextProvider>
     </PersistGate>
   </Provider>
-)
+);
